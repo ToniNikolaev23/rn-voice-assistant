c0dd473a6b5a2c44298bc02eadda08a4c7a34996
Toni Stoyanov <toni@1forfit.com>
2023-08-01 15:22:32 +0300

    Add voice recognition and translate into text

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,82 @@
-import React, {useState} from 'react';
-import {Image, SafeAreaView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import Features from '../components/Features';
+import {dummyMessages} from '../constants';
+import Voice from '@react-native-community/voice';
 
 const HomeScreen = () => {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(dummyMessages);
+  const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] = useState(false);
+  const [result, setResult] = useState('');
+
+  const speechStartHandler = e => {
+    console.log('Speech start handler');
+  };
+
+  const speechEndHandler = e => {
+    setRecording(false);
+    console.log('Speech end handler');
+  };
+
+  const speechResultsHandler = e => {
+    console.log('voice event:', e);
+    const text = e.value[0];
+    setResult(text);
+  };
+
+  const speechErrorHandler = e => {
+    console.log('speech error handler', e);
+  };
+
+  const clearHandler = () => {
+    setMessages([]);
+  };
+
+  const stopSpeakingHandler = () => {
+    setSpeaking(false);
+  };
+
+  const startRecording = async () => {
+    setRecording(true);
+    try {
+      await Voice.start('en-GB');
+    } catch (e) {
+      console.log('error', e);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      await Voice.stop();
+      setRecording(false);
+      //Fetch results
+    } catch (e) {
+      console.log('error', e);
+    }
+  };
+  useEffect(() => {
+    Voice.onSpeechStart = speechStartHandler;
+    Voice.onSpeechEnd = speechEndHandler;
+    Voice.onSpeechResults = speechResultsHandler;
+    Voice.onSpeechError = speechErrorHandler;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
   return (
     <View className="flex-1 bg-white">
       <SafeAreaView className="flex-1 flex mx-5">
@@ -17,7 +86,104 @@ const HomeScreen = () => {
             style={{height: hp(15), width: hp(15)}}
           />
         </View>
-        {messages.length > 0 ? <View>Messages</View> : <Features />}
+        {messages.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text
+              style={{fontSize: wp(5)}}
+              className="text-gray-700 font-semibold ml-1">
+              Assistant
+            </Text>
+            <View
+              style={{height: hp(58)}}
+              className="bg-neutral-200 rounded-3xl p-4">
+              <ScrollView
+                bounces={false}
+                className="space-y-4"
+                showsVerticalScrollIndicator={false}>
+                {messages.map((message, index) => {
+                  if (message.role === 'assistant') {
+                    if (message.content.includes('https')) {
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                            <Image
+                              source={{uri: message.content}}
+                              className="rounded-2xl"
+                              resizeMode="contain"
+                              style={{height: wp(60), width: wp(60)}}
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      return (
+                        <View
+                          key={index}
+                          style={{width: wp(70)}}
+                          className="bg-emerald-100 rounded-xl p-2 rounded-tl-none">
+                          <Text>{message.content}</Text>
+                        </View>
+                      );
+                    }
+                  } else {
+                    //use input
+                    return (
+                      <View key={index} className="flex-row justify-end">
+                        <View
+                          style={{width: wp(70)}}
+                          className="bg-white rounded-xl p-2 rounded-tr-none">
+                          <Text>{message.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                  //   return (
+                  //     <View>
+                  //       <Text>{message.content}</Text>
+                  //     </View>
+                  //   );
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
+
+        <View className="flex justify-center items-center">
+          {recording ? (
+            <TouchableOpacity onPress={stopRecording}>
+              <Image
+                className="rounded-full"
+                source={require('../../assets/images/voiceLoading.gif')}
+                style={{width: hp(10), height: hp(10)}}
+              />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={startRecording}>
+              <Image
+                className="rounded-full"
+                source={require('../../assets/images/recordingIcon.png')}
+                style={{width: hp(10), height: hp(10)}}
+              />
+            </TouchableOpacity>
+          )}
+          {messages.length > 0 && (
+            <TouchableOpacity
+              onPress={clearHandler}
+              className="bg-neutral-400 rounded-3xl p-2 absolute right-10">
+              <Text className="text-white font-semibold">Clear</Text>
+            </TouchableOpacity>
+          )}
+
+          {speaking && (
+            <TouchableOpacity
+              onPress={stopSpeakingHandler}
+              className="bg-red-400 rounded-3xl p-2 absolute left-10">
+              <Text className="text-white font-semibold">Stop</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </SafeAreaView>
     </View>
   );
